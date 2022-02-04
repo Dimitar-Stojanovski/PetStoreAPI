4f9373727a5f2467a25348687fdaf18c4fd174df
Dimitar-Stojanovski <dimitar.stojanovski77@gmail.com>
2022-02-04 07:01:08 +0100

    Commit

--- PetStoreAPI/Tests/PetTests/GetAPet.cs
+++ PetStoreAPI/Tests/PetTests/GetAPet.cs
@@ -35,5 +35,29 @@ namespace PetStoreAPI.Tests.PetTests
 
             });
         }
+
+
+        [Test]
+        public async Task GetAPetFromPostTest()
+        {
+            Assert.Multiple(async () =>
+            {
+                var api = new RestResponses();
+                var response = await api.GetResponse("pet/33");
+                Assert.AreEqual(200, (int)response.StatusCode);
+                var content = ModifyContent.DeserializeJson<AddAPetBody>(response);
+                Assert.AreEqual(content.id, 33);
+                Assert.AreEqual(content.category.id, 31);
+                Assert.AreEqual(content.category.name, "Doggie");
+                Assert.AreEqual(content.photoUrls[0], "Picture 1");
+                Assert.AreEqual(content.photoUrls[1], "Picture 2");
+                Assert.AreEqual(content.tags[0].id, 22);
+                Assert.AreEqual(content.tags[0].name, "Doggie");
+                Assert.AreEqual(content.tags[1].id, 33);
+                Assert.AreEqual(content.tags[1].name, "Dogo");
+                Assert.AreEqual(content.status, "available");
+
+            });
+        }
     }
 }
